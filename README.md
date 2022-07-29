236b44bc8915b360b1024f6d7bb6e5c677455719
DrantDumani <darnjames@gmail.com>
2022-07-29 12:07:41 -0400

    Completed form validation exercise and added README file

---
+++ README.md
@@ -0,0 +1,5 @@
+# Form Validated with JS Constraint Validation API
+
+This exercise required me to create a form that only relied on the Constraint Validation API provided by JavaScript. It uses live validation and helpful error messages to let the user know if what they're typing is correct before they submit the form. 
+
+Exercise available on [The Odin Project](https://www.theodinproject.com/lessons/node-path-javascript-form-validation-with-javascript)
--- script.js
+++ script.js
@@ -1,33 +1,164 @@
-const inputs = document.querySelectorAll('.input-box>input');
-inputs.forEach((elem) => {
-  requireInput(elem);
-  elem.addEventListener('focusout', () => {
-    elem.setCustomValidity('');
-    if (!elem.valid) {
-      const validState = elem.dataset.objKey;
+const validityObjs = {};
+const form = document.querySelector('.form-container');
+
+function validateForm(e) {
+  e.preventDefault();
+  const victoryText = document.querySelector('.victory-text');
+  victoryText.classList.remove('hide');
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  validateForm(e);
+});
+
+function assignValidityProps(elem, validityObj) {
+  const keys = Object.keys(validityObj.props);
+  const { key: keyStr } = validityObj;
+  keys.forEach((prop) => {
+    elem[prop] = validityObj.props[prop];
+  });
+  elem.dataset.key = keyStr;
+  validityObjs[keyStr] = validityObj;
+}
+
+const emailValidity = {
+  props: {
+    maxLength: 254,
+    type: 'email',
+  },
+  msgPropPairs: [
+    { propCheck: 'tooLong', msg: 'Email must be shorter than 255 characters' },
+    { propCheck: 'typeMismatch', msg: 'Please enter a valid email' },
+  ],
+  key: 'email',
+};
+
+const zipCodeValidity = {
+  props: { pattern: '^\\d{5}$' },
+  msgPropPairs: [
+    { propCheck: 'patternMismatch', msg: 'Zip code must be 5 numbers' },
+  ],
+  key: 'zipCode',
+};
+
+const passwordValidity = {
+  props: {
+    pattern: '.*(?=.*?[\\W_])(?=.*?\\d).*',
+    minLength: 6,
+    maxLength: 20,
+  },
+  msgPropPairs: [
+    {
+      propCheck: 'patternMismatch',
+      msg: 'Password must contain a special character, a letter, and a number',
+    },
+    {
+      propCheck: 'tooLong',
+      msg: 'Password cannot be longer than 20 characters',
+    },
+    {
+      propCheck: 'tooShort',
+      msg: 'Password must not be shorter than 6 characters',
+    },
+  ],
+
+};
+
+const countryValidity = {
+  props: {
+    maxLength: 56,
+    pattern: '^[A-Z][a-z]*$',
+  },
+  msgPropPairs: [
+    { propCheck: 'patternMismatch', msg: 'Country name must start with a capital letter' },
+    { propCheck: 'tooLong', msg: 'Country name cannot be longer then 56 characters.' },
+  ],
+  key: 'country',
+};
+
+function handleValidationOnNoFocus(e) {
+  const elem = e.target;
+  const { id } = elem;
+  const validityState = elem.validity;
+  if (!validityState.valid) {
+    const errorMsg = document.querySelector(`#${id} + .validation-text`);
+    const { key } = elem.dataset;
+    let text = '';
+    const { msgPropPairs } = validityObjs[key];
+    msgPropPairs.forEach((obj) => {
+      if (validityState[obj.propCheck]) {
+        text = obj.msg;
+        elem.setCustomValidity(obj.msg);
+      }
+    });
+    if (validityState.valueMissing) {
+      text = 'Please fill in this field';
     }
+    errorMsg.innerText = text;
+    elem.reportValidity();
+  }
+}
+
+function handleValidationOnInput(e) {
+  const elem = e.target;
+  elem.setCustomValidity('');
+  const { id } = elem;
+  const validityState = elem.validity;
+  if (validityState.valid) {
+    const errorMsg = document.querySelector(`#${id} + .validation-text`);
+    errorMsg.innerText = '';
+  }
+}
+
+const email = document.querySelector('#email');
+const country = document.querySelector('#country-name');
+const zipCode = document.querySelector('#zipCode');
+const password = document.querySelector('#password');
+
+const pairs = [
+  [email, emailValidity],
+  [country, countryValidity],
+  [zipCode, zipCodeValidity],
+  [password, passwordValidity],
+];
+
+pairs.forEach((el) => {
+  assignValidityProps(el[0], el[1]);
+  el[0].required = true;
+  el[0].addEventListener('focusout', (e) => {
+    handleValidationOnNoFocus(e);
+  });
+  el[0].addEventListener('input', (e) => {
+    handleValidationOnInput(e);
   });
 });
 
-function requireInput(inputElem) {
-  inputElem.required = true;
+const confirmPassword = document.querySelector('#confirm-password');
+confirmPassword.required = true;
+
+function validateConfirmPass() {
+  const passwordVal = document.querySelector('#password').value;
+  const confirmPass = document.querySelector('#confirm-password');
+  if (confirmPass.value !== '') {
+    if (confirmPass.value !== passwordVal) {
+      confirmPass.setCustomValidity('Passwords must match');
+      confirmPass.reportValidity();
+      const errMsg = document.querySelector(`#${confirmPass.id} + .validation-text`);
+      errMsg.innerText = 'Passwords must match';
+    }
+  }
 }
 
-function assignValidation(inputForm, validationObj) {
-  const keys = Object.keys(validationObj);
-  keys.forEach((key) => {
-    inputForm.key = validationObj.key;
-  });
+function validateConfirmPassOnInput() {
+  const passwordVal = document.querySelector('#password').value;
+  const confirmPass = document.querySelector('#confirm-password');
+  if (confirmPass.value === passwordVal) {
+    confirmPass.setCustomValidity('');
+    const errMsg = document.querySelector(`#${confirmPass.id} + .validation-text`);
+    errMsg.innerText = '';
+  }
 }
 
-const countryValid = {
-  pattern: /^[A-Z]/,
-};
-
-const zipCodeValid = {
-  pattern: /d{5}/,
-};
-
-const passwordValid = {
-  pattern: /^(?=.*?[\W_])(?=.*?\d).{6,}$/,
-};
+confirmPassword.addEventListener('focusout', validateConfirmPass);
+confirmPassword.addEventListener('input', validateConfirmPassOnInput);
